b3d1aba876fd6739c1dd0b833683e7287f101b42
Ирина <70brigantina@gmail.com>
2026-08-26 23:57:41 +0300

    Добавить персональные аффирмации, редактирование цели и яркую иконку (#4)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='transformation-journal-v5';
-const ASSETS=['./','./index.html','./styles.css','./app.js?v=5','./manifest.webmanifest','./icon-192.png','./icon-512.png','./splash-750x1334.png'];
+const CACHE='transformation-journal-v6';
+const ASSETS=['./','./index.html','./styles.css','./app.js?v=6','./manifest.webmanifest','./icon-192.png','./icon-512.png','./splash-750x1334.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{
